981e437342d5d6cfedc56e4db4397f1e2888bc78
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-15 18:06:02 +0100

    rev 3
    	modified:   quadequi/main.c
    
    	- work on initialization code of quadequi

--- quadequi/main.c
+++ quadequi/main.c
@@ -1,9 +1,33 @@
 
 #include <stdio.h>
+#include <string.h>
 #include <libdanh/danh.h>
 #include <libdanh/danhmath.h>
 
+int quadequi_release = 0;
+int quadequi_revision = 0;
+char* quadequi_version = "0.0 2011-03-15";
+
 int main (int argc, char **argv){
-	printf("libdanh version: %s\n", libdanh_version);
+	int i = 0;
+	int print_version = 0;
+	int debug = 0;
+	int print_help = 0;
+	while (i < argc){
+		if (strcmp(argv[i], "-V") == 0 || strcmp(argv[i], "--version") == 0) print_version = 1;
+		if (strcmp(argv[i], "--debug") == 0) debug = 1;
+		if (strcmp(argv[i], "--help") == 0) print_help = 1;
+		i++;
+	}
+	
+	if (print_help == 1 || argc == 1) puts("Usage: quadequi [--help] [--debug] [-V | --version] x y");
+
+	if (print_version == 1) {
+		printf("libdanh version: %s\n", libdanh_version);
+		printf("quadequi version: %s\n", quadequi_version);
+	}
+
+	if (debug == 1) puts("debug mode is ON");
+
 	return 0;
 }
