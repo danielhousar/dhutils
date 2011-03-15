3486a13ca41afc3e53cb1933e0b88c13b7e7b02d
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-15 16:15:20 +0100

    rev 1
    	modified:   quadequi/main.c
    
    	- work on initializing code of quadequi

---
+++ quadequi/main.c
@@ -0,0 +1,9 @@
+
+#include <stdio.h>
+#include <libdanh/danh.h>
+#include <libdanh/danhmath.h>
+
+int main (int argc, char **argv){
+	printf("libdanh version: %s\n", libdanh_version);
+	return 0;
+}
