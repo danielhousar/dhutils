82341b4cb69ab3df437867c3404f3cf2c44be263
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-18 08:28:21 +0000

    [24] - fixed debug message
    	modified:   quadequi/main.c

--- quadequi/main.c
+++ quadequi/main.c
@@ -14,6 +14,8 @@ char* quadequi_version = "0.0 2011-03-17";
 int main (int argc, char **argv){
 	int i = 0;
 	int is_y = 0;
+	int x_is_set = 0;
+	int y_is_set = 0;
 	int print_version = 0;
 	int debug = 0;
 	int print_help = 0;
@@ -26,10 +28,12 @@ int main (int argc, char **argv){
 		if (strcmp(argv[i], "--help") == 0) print_help = 1;
 		if (argv[i][0] > 48 && argv[i][0] < 57 && is_y == 0){
 			x = str_to_double_dh(argv[i]);
+			x_is_set = 1;
 			is_y = 1;
 		}
 		if (argv[i][0] > 48 && argv[i][0] < 57 && is_y == 1) {
 			y = str_to_double_dh(argv[i]);
+			y_is_set = 1;
 		}
 		i++;
 	}
@@ -42,9 +46,10 @@ int main (int argc, char **argv){
 	}
 
 	if (debug == 1) puts("debug mode is ON");
-	if (debug == 1) printf("x: %lG\n", x);
-	if (debug == 1) printf("y: %lG\n", y);
-
+	if (debug == 1 && x_is_set == 1) printf("x: %lG\n", x);
+	if (x_is_set == 0) puts("x not set");
+	if (debug == 1 && y_is_set == 1) printf("y: %lG\n", y);
+	if (y_is_set == 0)puts("y not set");
 	return 0;
 }
 
