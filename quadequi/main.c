be888e852cafa4d2a9dc486d54f2d4ebaf2f1862
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-20 18:58:35 +0100

    [53] quadequi code simplification, removed some variables
    	modified:   quadequi/main.c

--- quadequi/main.c
+++ quadequi/main.c
@@ -13,9 +13,7 @@ char* quadequi_version = "0.3 2011-03-19";
 
 int main (int argc, char **argv){
 	int i = 0;
-	int a_is_set = 0;
-	int b_is_set = 0;
-	int c_is_set = 0;
+	int var_set = 0;
 	int print_version = 0;
 	int debug = 0;
 	int print_help = 0;
@@ -31,17 +29,15 @@ int main (int argc, char **argv){
 		if (strcmp_dh(argv[i], "--help") == 0) print_help = 1;
 
 		if (str_is_number_dh(argv[i]) == 1) {
-			if (a_is_set == 0){
+			var_set++;
+			if (var_set == 1){
 				a = str_to_double_dh(argv[i]);
-				a_is_set = 1;
 			}
-			else if (a_is_set == 1 && b_is_set == 0) {
+			else if (var_set == 2) {
 				b = str_to_double_dh(argv[i]);
-				b_is_set = 1;
 			}
 			else {
 				c = str_to_double_dh(argv[i]);
-				c_is_set = 1;
 			}
 		}
 
@@ -55,15 +51,15 @@ int main (int argc, char **argv){
 		printf("quadequi version: %s\n", quadequi_version);
 	}
 
-	if (a_is_set == 0) puts("a not set");
-	if (b_is_set == 0) puts("b not set");
-	if (c_is_set == 0) puts("c not set");
+	if (var_set < 1) puts("a not set");
+	if (var_set < 2) puts("b not set");
+	if (var_set < 3) puts("c not set");
 	if (debug == 1) puts("debug mode is ON");
-	if (debug == 1 && a_is_set == 1) printf("a: %lG\n", a);
-	if (debug == 1 && b_is_set == 1) printf("b: %lG\n", b);
-	if (debug == 1 && c_is_set == 1) printf("c: %lG\n", c);
+	if (debug == 1 && var_set >= 1) printf("a: %lG\n", a);
+	if (debug == 1 && var_set >= 2) printf("b: %lG\n", b);
+	if (debug == 1 && var_set >= 3) printf("c: %lG\n", c);
 	
-	if (a_is_set == 1 && b_is_set == 1 && c_is_set == 1){
+	if (var_set >= 3){
 		main_x1 = quadequi_double_dh(a, b, c, 1);
 		main_x2 = quadequi_double_dh(a, b, c, 2);
 
