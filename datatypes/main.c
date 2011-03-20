bd75c1e08fdf2e0a1fdc60e9fe9937a3b0786527
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-20 02:29:29 +0000

    [52] expanded datatypes code
    	modified:   datatypes/main.c

--- datatypes/main.c
+++ datatypes/main.c
@@ -14,5 +14,7 @@ int main (int argc, char **argv){
 	printf("size of long: %i\n", sizeof(long));
 	printf("size of float: %i\n", sizeof(float));
 	printf("size of double: %i\n", sizeof(double));
+	printf("size of long double: %i\n", sizeof(long double));
+	printf("size of size_t: %i\n", sizeof(size_t));
 	return 0;
 }
