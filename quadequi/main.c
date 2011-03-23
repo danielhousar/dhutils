3ffd758cbacbacff62bfb07d52a5aaacae5fd727
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-23 13:58:14 +0100

    [57] quadequi init code updated, little bit changed qe_init usage
    	modified:   quadequi/main.c

--- quadequi/main.c
+++ quadequi/main.c
@@ -22,9 +22,9 @@ int main (int argc, char **argv){
 	double main_x2;
 
 	while (i < argc){
-		if (strcmp_dh(argv[i], "--help") == 0) qe_init = 1;
-		if (strcmp_dh(argv[i], "--version") == 0) qe_init = 2;
-		if (strcmp_dh(argv[i], "--debug") == 0) qe_init = 4;
+		if (strcmp_dh(argv[i], "--help") == 0) qe_init += 1;
+		if (strcmp_dh(argv[i], "--version") == 0) qe_init += 2;
+		if (strcmp_dh(argv[i], "--debug") == 0) qe_init += 4;
 		
 		if (str_is_number_dh(argv[i]) == 1) {
 			var_set++;
@@ -42,21 +42,21 @@ int main (int argc, char **argv){
 		i++;
 	}
 	
-	if (qe_init == 1 || argc == 1) puts("Usage: quadequi [--help] [--debug] [--version] a b c");
+	if (qe_init == 1 || argc == 1) puts("Usage: quadequi [--help]|[--debug] [--version] a b c");
 
 	if (qe_init > 1) {
 		printf("libdanh version: %s\n", libdanh_version);
 		printf("quadequi version: %s\n", quadequi_version);
 	}
-	if (qe_init > 2) puts("debug mode is ON");
+	if (qe_init > 3) puts("debug mode is ON");
 	
 	if (var_set < 1) puts("a not set");
 	if (var_set < 2) puts("b not set");
 	if (var_set < 3) puts("c not set");
 	
-	if (qe_init > 2 && var_set >= 1) printf("a: %lG\n", a);
-	if (qe_init > 2 && var_set >= 2) printf("b: %lG\n", b);
-	if (qe_init > 2 && var_set >= 3) printf("c: %lG\n", c);
+	if (qe_init > 3 && var_set >= 1) printf("a: %lG\n", a);
+	if (qe_init > 3 && var_set >= 2) printf("b: %lG\n", b);
+	if (qe_init > 3 && var_set >= 3) printf("c: %lG\n", c);
 	
 	if (var_set >= 3){
 		main_x1 = quadequi_double_dh(a, b, c, 1);
@@ -64,7 +64,7 @@ int main (int argc, char **argv){
 
 		printf("x1 = %lG\nx2 = %lG\n", main_x1, main_x2);
 	}
-	else puts("not all required values given - stop processing");
+	else if (var_set > 0) puts("not all required values given - stop processing");
 
 	return 0;
 }
