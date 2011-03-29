0c64e9574ad5b853166357268225bb37c52cbcd6
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-29 22:19:41 +0000

    [111] datatypes 0.2, added sizeof(short)
    	modified:   README
    	modified:   datatypes/main.c

--- datatypes/main.c
+++ datatypes/main.c
@@ -10,12 +10,19 @@
 //#include "../include/types.h"
 
 int main (int argc, char **argv){
-	printf("size of int: %i\n", sizeof(int));
-	printf("size of long: %i\n", sizeof(long));
-	printf("size of long long: %i\n", sizeof(long long));
-	printf("size of float: %i\n", sizeof(float));
-	printf("size of double: %i\n", sizeof(double));
-	printf("size of long double: %i\n", sizeof(long double));
+	puts("datatypes 0.2 2011-03-29");
+	puts(" ");
+	puts("----- ENVIRONMENT -----");
+	puts(" ");
+	puts("sizeof()");
+	printf("short: %i\n", sizeof(short));
+	printf("int: %i\n", sizeof(int));
+	printf("long: %i\n", sizeof(long));
+	printf("long long: %i\n", sizeof(long long));
+	printf("float: %i\n", sizeof(float));
+	printf("double: %i\n", sizeof(double));
+	printf("long double: %i\n", sizeof(long double));
 	printf("size of size_t: %i\n", sizeof(size_t));
+	puts(" ");
 	return 0;
 }
