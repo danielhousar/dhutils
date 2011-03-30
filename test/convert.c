317b83283843dfecaadcbaae3379cfe6b70f1087
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-03-30 03:15:50 +0000

    [116] deleted split test
    	modified:   README
    	modified:   test/Makefile
    	modified:   test/convert.c
    	deleted:    test/split.c

--- test/convert.c
+++ test/convert.c
@@ -3,7 +3,7 @@
 #include "../include/types.h"
 
 int main () {
-	long testvar = shexrep_to_li_dh("0xFf");
+	long testvar = shexrep_to_li_dh("1xaf");
 	printf("testvar = %i\n", testvar);
 	return 0;
 }
