c6407386f6f760991cc08e296c9332ce5bd24341
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-04-02 04:46:29 +0000

    [127] sdecrep_to_fd_dh() code optimization and simplification
    	modified:   libdanh/types.c
    	modified:   test/convert.c

--- libdanh/types.c
+++ libdanh/types.c
@@ -6,39 +6,36 @@
 
 double sdecrep_to_fd_dh(char* s){
 	double retval = 0.0;
-	int char_count = 0;
 	int i = 0;
 	double divider = 1.0;
 	int numbers[48];
-	int is_negative = 0;
+	int sign = 1.0;
 	int was_point = 0;
 
-	while (s && s[char_count] != 0 && char_count < 48) {
-		numbers[char_count] = s[char_count] - 48;
-		char_count++;
-	}
+	if (s[0] == 45) { i = 1; sign = -1.0; }
 
-	if (numbers[0] == -3) { i = 1; is_negative = 1; }
+	while (s && s[i] != 0 && i < 48) {
+		numbers[i] = s[i] - 48;
 
-	while (i < char_count){
-		if (numbers[i] >= 0 && numbers[i] <= 9 && was_point == 0){
-			retval *= 10.0;
-			retval += numbers[i];
-			i++;
+		if (numbers[i] >= 0 && numbers[i] <= 9){
+			if (was_point == 0) {
+				retval *= 10.0;
+				retval += numbers[i];
+			}
+			else {
+				divider *= 10.0;
+				retval += numbers[i] / divider;
+			}
 		}
-		else if (numbers[i] == -2){
+
+		if (numbers[i] == -2){
 			was_point = 1;
-			i++;
 		}
-		else {
-			divider *= 10.0;
-			retval += numbers[i] / divider;
-			i++;
-		}
+
+		i++;
 	}
 
-	if (is_negative == 1)
-		retval *= -1.0;
+	retval *= sign;
 
 	return retval;
 }
--- test/convert.c
+++ test/convert.c
@@ -3,7 +3,9 @@
 #include "../include/types.h"
 
 int main () {
-	long testvar = shexrep_to_li_dh("1xaf");
-	printf("testvar = %i\n", testvar);
+	long l = shexrep_to_li_dh("1xaf");
+	double d = sdecrep_to_fd_dh("2.0");
+	printf("l = %i\n", l);
+	printf("d = %f\n", d);
 	return 0;
 }
