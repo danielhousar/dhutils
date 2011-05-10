f4c0d28020d32f3e108a94a0544fdc88cf354b88
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-05-10 13:38:13 +0000

    test/convert: minor changes in code

--- test/convert.c
+++ test/convert.c
@@ -3,12 +3,9 @@
 #include "../include/types.h"
 
 int main () {
-	long l = shexrep_to_li_dh("1xaf");
-	double d = sdecrep_to_fd_dh("2.0");
-	unsigned int ui = sbinrep_to_ui_dh("00011000");
-	printf("l = %i\n", l);
-	printf("d = %f\n", d);
-	printf("ui = %u\n", ui);
+	printf("li = %i\n", shexrep_to_li_dh("1xaf"));
+	printf("fd = %f\n", sdecrep_to_fd_dh("2.0"));
+	printf("ui = %u\n", sbinrep_to_ui_dh("00011000"));
 	print_ui_to_sbinrep_dh(20);
 	return 0;
 }
