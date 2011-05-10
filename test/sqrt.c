157ac1783231ce84e0cb7cb42d3725bb06ca3dac
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-05-10 15:11:17 +0000

    test/sqrt: changes in code

--- test/sqrt.c
+++ test/sqrt.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-#include "../include/danhmath.h"
-#include "../include/danh.h"
+#include "../include/dhmath.h"
+#include "../include/dh.h"
 #include "../include/types.h"
 
 int main (int argc, char **argv){
 	double x;
-	printf("libdanh %s\n", dh_version_string);
+	printf("libdh %s\n", dh_version_string);
 	if (argc > 1){
 		x = sdecrep_to_fd_dh(argv[1]);
+		double root = sqrt_fd_dh(x);
+		printf("%.*f\n", 10, sqrt_fd_dh(x));
 	}
-	else scanf("%lG", &x);
-	double root = sqrt_fd_dh(x);
-	printf("%.*f\n", 10, root);
+	return 0;
 }
