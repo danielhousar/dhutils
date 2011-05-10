13994bd7d35986cf7691ea62eeb01e66fa7941d8
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-05-11 01:03:44 +0200

    [207] quadequi: minor change in initial code

--- quadequi/main.c
+++ quadequi/main.c
@@ -48,7 +48,7 @@ int main (int argc, char **argv){
 
 /* output */
 	if (qe_init == 1 || argc == 1) {
-		puts("Usage: quadequi [--help]|[--debug] [--version] a b c");
+		puts("Usage: %s [--help]|[--debug] [--version] a b c", argv[0]);
 		return 0;
 	}
 
