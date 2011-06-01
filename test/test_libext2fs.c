361318ec5cdfc83a651b3c628f31213c37f7c281
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-06-02 00:26:51 +0200

    modified: test/test_libext2fs.c

--- test/test_libext2fs.c
+++ test/test_libext2fs.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
-#include "../../e2fsprogs/lib/ext2fs/ext2fs.h"
-
+#define EXT2_FLAT_INCLUDES
+#include "../../e2fsprogs_win32/lib/ext2fs/com_err.h"
+#include "../../e2fsprogs_win32/lib/ext2fs/ext2_io.h"
+#include "../../e2fsprogs_win32/lib/ext2fs/ext2_err.h"
+#include "../../e2fsprogs_win32/lib/ext2fs/ext2_ext_attr.h"
+#include "../../e2fsprogs_win32/lib/ext2fs/ext2fs.h"
 int main (int argc, char **argv){
 	puts("testing...");
 	return 0;
