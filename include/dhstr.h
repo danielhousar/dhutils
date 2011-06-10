f8aba5b65e8246e2c7952f0b89ad5bdc981f0e17
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-06-10 22:33:41 +0200

    [257] include/dhstr.h: fixed declaration

--- include/dhstr.h
+++ include/dhstr.h
@@ -19,7 +19,7 @@ extern long shexrep_to_li_dh(char* s);
 
 extern int snumrep_to_i_dh(char *s);
 
-unsigned int sbinrep_to_ui_dh(char *s);
+extern unsigned int sbinrep_to_ui_dh(char *s);
 
 extern int s_is_num_dh(char* s);
 
