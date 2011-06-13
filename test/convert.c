b77f3a87283b62b2b3b4e9fc640324728dec594a
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-06-13 17:04:23 +0000

    updated test/convert.c

--- test/convert.c
+++ test/convert.c
@@ -4,7 +4,7 @@
 #include "../include/dhmisc.h"
 
 int main () {
-	printf("li = %li\n", shexrep_to_li_dh("1xaf"));
+	printf("li = %li\n", shexrep_to_li_dh("0xaf"));
 	printf("%x\n", 175);
 	printf("fd = %f\n", sdecrep_to_fd_dh("2.0"));
 	printf("ui = %u\n", sbinrep_to_ui_dh("00011000"));
