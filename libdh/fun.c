4be2513fdc6d25f34fb63f91adc65139d6c7370e
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-06-13 12:35:18 +0000

    added libdh/fun.c and include/dhfun.h

---
+++ libdh/fun.c
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * file:	libdh/fun.c
+ *
+ * description:	various functions for fun
+ *
+ ***************************************************************************** */
+
+#include "../include/dhfun.h"
+
+/* mad_d_dh()
+ * Returns ((2 * a) / 3).
+ * In the fact: the main loop converges to ((2 * a) / 3), but the return value
+ * depends on count of loops.
+ * Do you know more stupid algorithm?
+ */
+double mad_d_dh(double a){
+	double retval = a;
+	double helper1 = a;
+	double helper2 = 0;
+	int counter = 0;
+	while (counter < 30){
+		retval = (helper1 + helper2) / 2.00;
+		helper2 = retval;
+		helper1 = (helper1 + helper2) / 2.00;
+		retval = helper1;
+		counter++;
+	}
+	return retval;
+}
+
