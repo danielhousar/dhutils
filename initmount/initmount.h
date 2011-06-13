abc73f78c0d44347dd612388afb697dc867901a5
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-06-13 17:44:41 +0000

    [267] work on next utility

---
+++ initmount/initmount.h
@@ -0,0 +1,15 @@
+/*****************************************************************************
+ * file:	initmount/initmount.h
+ ***************************************************************************** */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "config.h"
+
+#ifndef DHIM_H
+#define DHIM_H
+
+extern const char *im_version_str = "0.1";
+
+#endif
+
