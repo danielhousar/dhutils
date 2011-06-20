1c17732d6f627b08df1e1cd1c43e79f22f509227
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-06-20 10:41:19 -0700

    [280] initmount: removed includes and printf() call

--- initmount/main.c
+++ initmount/main.c
@@ -2,22 +2,15 @@
  * file:	initmount/main.c
  ***************************************************************************** */
 
-#include <unistd.h>
-#include <stdio.h>
-#include <errno.h>
-#include <fcntl.h>
-
-#include <sys/types.h>
 #include <sys/stat.h>
 #include <sys/mount.h>
-
 #include <mntent.h>
 
 #ifndef MS_RELATIME
 #define MS_RELATIME 0x200000
 #endif
 
-int main (int argc, char **argv) {
+int main () {
 	int result = 0;
 
 	umask(022);
@@ -28,8 +21,6 @@ int main (int argc, char **argv) {
 	result += mount ("devpts", "/dev/pts", "devpts", MS_RELATIME, "");
 	result += mount ("/dev/sda4", "/", "ext4", MS_REMOUNT|MS_RDONLY, "");
 	result += mount ("/dev/sda4", "/", "ext4", MS_REMOUNT, "");
-
-	printf("initmount result %i\n", result);
 
 	return result;
 }
