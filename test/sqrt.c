1483bc56d5a3b540713e276f39323a2bbafb28f3
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-08-10 11:07:51 -0700

    little work in test

--- test/sqrt.c
+++ test/sqrt.c
@@ -4,11 +4,28 @@
 #include "../include/dhstr.h"
 
 int main (int argc, char **argv){
-	double x;
+	int i = 0;
+	double x = 0;
+	double y = 0;
+	double z = 4;
+	double d = 1;
+
 	//printf("libdh %s\n", dh_version_str);
+
 	if (argc > 1){
 		x = sdecrep_to_fd_dh(argv[1]);
-		printf("%.*f\n", 20, sqrt_dh(x));
+		printf("%.*f\n", 20, x);
 	}
+/*
+
+*/
+	while (i < 300) {
+		z /= 2;
+		d *= 2;
+		y = y + (z/d);
+		i++;
+	}
+
+	printf("%.*f\n", 20, y);
 	return 0;
 }
