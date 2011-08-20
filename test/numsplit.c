37e78b434f41c05b992219274971f12d006f09a3
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-08-20 13:36:33 +0200

    test: experimental work on numsplit

---
+++ test/numsplit.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+
+int main (int argc, char **argv) {
+	int pos = 0;
+	int x = 1;
+	int num = 453872;
+	int numarray[6] = { 0, 0, 0, 0, 0, 0 };
+
+	while (pos < 6) {
+		while ((num - i) ) {
+			i++;
+		}
+		numarray[pos] = i;
+		pos++;
+	}
+
+	return 0;
+}
+
+
