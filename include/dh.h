f8459e5f072e4d2c0f11f37141e6b8d787a286d6
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-08-22 00:56:25 +0200

    [293] libdh: changed version info format, now 0.9 20110822.293

--- include/dh.h
+++ include/dh.h
@@ -8,11 +8,13 @@
 #define LIBDH
 #define LIBDH_VERSION 0
 #define LIBDH_RELEASE 9
-#define LIBDH_REVISION 20110810
+#define LIBDH_REV_DATE 20110810
+#define LIBDH_REV_NUM
 
 extern const int dh_version;
 extern const int dh_release;
-extern const int dh_rev;
+extern const int dh_rev_date;
+extern const int dh_rev_num;
 
 #endif
 
--- libdh/dh.c
+++ libdh/dh.c
@@ -6,5 +6,6 @@
 
 const int dh_version = LIBDH_VERSION;
 const int dh_release = LIBDH_RELEASE;
-const int dh_rev = LIBDH_REVISION;
+const int dh_rev_date = LIBDH_REV_DATE;
+const int dh_rev_num = LIBDH_REV_NUM;
 
