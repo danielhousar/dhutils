ec4e4bee871ddce0601cf7c9c75c681c9d218f55
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-08-22 04:56:30 +0200

    [294] (fix) include/dh.h: compillation fix, added missing revision number

--- include/dh.h
+++ include/dh.h
@@ -8,8 +8,8 @@
 #define LIBDH
 #define LIBDH_VERSION 0
 #define LIBDH_RELEASE 9
-#define LIBDH_REV_DATE 20110810
-#define LIBDH_REV_NUM
+#define LIBDH_REV_DATE 20110822
+#define LIBDH_REV_NUM 294
 
 extern const int dh_version;
 extern const int dh_release;
