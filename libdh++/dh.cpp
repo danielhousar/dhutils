08754756af9667d60ef14a41590b279ae7c40e47
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-08-22 07:11:55 +0200

    (devel) libdh++: initial work on C++ version of libdh

---
+++ libdh++/dh.cpp
@@ -0,0 +1,11 @@
+/*****************************************************************************
+ * file:	libdh++/dh.cpp
+ ***************************************************************************** */
+
+#include "../include/dh.h"
+
+const int dh_version = LIBDH_VERSION;
+const int dh_release = LIBDH_RELEASE;
+const int dh_rev_date = LIBDH_REV_DATE;
+const int dh_rev_num = LIBDH_REV_NUM;
+
