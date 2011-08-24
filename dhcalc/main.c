61f2bda855c12ebdabb09682e8b6f94e31c8f963
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-08-24 23:00:58 +0200

    (devel) dhcalc: little work done on args handling and added testing code for 'add' action

--- dhcalc/main.c
+++ dhcalc/main.c
@@ -5,35 +5,46 @@
 #include "../include/dhstr.h"
 
 #define ADD		1
-#define MINUS		2
+#define MINUS	2
 #define MULTIPLE	3
-#define DIVIDE		4
+#define DIVIDE	4
 #define INT		8
-#define FLOAT		16
+#define FLOAT	16
+
+
+
+int dhc_add_i() {
+	return 0;
+}
 
 /* main */
 
 int main(int argc, char **argv) {
 	int action = 0;
 	char action_lock = 0;
-	int arg_type = 0;
 	int arg = 1;
-
+	int args_i[64];
+	int args_i_count = 0;
 #ifdef DH_DEBUG
 	printf("start main()\n");
 #endif
 
 /* init */
-	while (arg < argc) {
-		if (!strcmp_dh(argv[arg], "+") && !action_lock) { action = 1; action_lock = 1; }
-		if (!strcmp_dh(argv[arg], "-") && !action_lock) { action = 2; action_lock = 1; }
-		if (!strcmp_dh(argv[arg], "*") && !action_lock) { action = 3; action_lock = 1; }
-		if (!strcmp_dh(argv[arg], "/") && !action_lock) { action = 4; action_lock = 1; }
-		if (s_is_num_dh(argv[arg]) == 1) arg_type = 8;
-
-		arg++;
+	for (arg = 1; arg < argc; arg++) {
+		if (!strcmp_dh(argv[arg], "+") && !action_lock) { action = 1; action_lock = 1; continue; }
+		if (!strcmp_dh(argv[arg], "-") && !action_lock) { action = 2; action_lock = 1; continue; }
+		if (!strcmp_dh(argv[arg], "*") && !action_lock) { action = 3; action_lock = 1; continue; }
+		if (!strcmp_dh(argv[arg], "/") && !action_lock) { action = 4; action_lock = 1; continue; }
+		if (s_is_num_dh(argv[arg])) { args_i[args_i_count] = snumrep_to_i_dh(argv[arg]); args_i_count++; }
 	}
 
+	switch (action) {
+	case 1:
+		printf("%d + %d = %d\n", args_i[0], args_i[1], args_i[0] + args_i[1]);
+		break;
+	default:
+		break;
+	}
 #ifdef DH_DEBUG
 	printf("end main() on return 0\n");
 #endif
