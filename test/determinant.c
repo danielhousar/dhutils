71cdad7fd5c840f008e8be6ddabc62df2b7fed71
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-11-21 23:19:02 +0100

    test: initial work on app for determinant computing

---
+++ test/determinant.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <math.h>
+#include <complex.h>
+#include "../include/dh.h"
+#include "../include/dhmath.h"
+#include "../include/dhstr.h"
+
+//typedef double[2] complex
+
+int main(int argc, char **argv) {
+	_Complex double a = 2.0+1.0fi;
+
+	printf("a = %f + %fi\n", __real__ a, __imag__ a);
+	printf("a = %g\n", a);
+	//int i = 0;
+
+	//while (argc > 1 && i) {
+
+	//}
+	return 0;
+}
