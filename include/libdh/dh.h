da2da93d6cfec504fd3e1519db179089fe224242
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-11-22 11:49:23 +0100

    [310] added internal headers in libdh
    	new file:   libdh/dh.h
    	new file:   libdh/dhfun.h
    	new file:   libdh/dhmath.h
    	new file:   libdh/dhmemset.h
    	new file:   libdh/dhmisc.h
    	new file:   libdh/dhstr.h

---
+++ include/libdh/dh.h
@@ -0,0 +1,20 @@
+/***************************************************************
+ * file:	include/dh.h
+ *************************************************************** */
+
+#ifndef DH_H
+
+#define DH_H
+#define LIBDH
+#define LIBDH_VERSION 0
+#define LIBDH_RELEASE 9
+#define LIBDH_REV_DATE 20111011
+#define LIBDH_REV_NUM 305
+
+extern const int dh_version;
+extern const int dh_release;
+extern const int dh_rev_date;
+extern const int dh_rev_num;
+
+#endif
+
---
+++ include/libdh/dhmath.h
@@ -0,0 +1,38 @@
+/******************************************************
+ * file:	include/dhmath.h
+ ****************************************************** */
+
+#ifndef DHMATH_H
+#define DHMATH_H
+
+#include <math.h>
+#include <complex.h>
+
+#define PI_dh 3.14159265358979323846
+#define E_dh 2.7182818284590452354
+
+extern double qe_roots[2];
+
+/*
+#ifdef HAVE_MATH_H
+extern __float128 factorial_f128_dh(__float128 a);
+#endif
+*/
+
+extern long double factorial_dh(long double a);
+extern float sqrtf_dh(float a);
+extern double sqrt_dh(double a);
+extern long double sqrt_fld_dh(long double a);
+extern double *quadequi_dh(double a, double b, double c);
+extern double quadequi_1_dh(double a, double b, double c);
+extern double quadequi_2_dh(double a, double b, double c);
+extern long double sin_deg_dh(long double a);
+extern long double sin_dh(long double a);
+extern long double cos_dh(long double ang);
+extern inline long double radtodeg_dh(long double a);
+extern inline long double degtorad_dh(long double a);
+extern inline long double degtorad_spec_dh(long double a);
+extern long double pow_dh(long double a, long long exp);
+
+#endif
+
