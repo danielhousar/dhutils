8f104ff8bf54fc9621d3c5e01094733b1317f6b2
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-11-29 10:44:04 +0100

    utils/shq.c: added printf() about action

--- utils/shq.c
+++ utils/shq.c
@@ -6,10 +6,11 @@
 
 int main (int argc, char **argv) {
 	int i = 0;
-	scanf("%i", &i);
-	printf("%i\n", i);
-	if (argc > 1 && i==1) {
-		system(argv[1]);
+	if (argc > 1) {
+		printf("do: %s ?\n", argv[1]);
+		scanf("%i", &i);
+		if (i==1)
+			system(argv[1]);
 		return 0;
 	}
 	else return 1;
