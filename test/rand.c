26846f1deabf886cc36ff85f8432b0edead241c9
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-11-30 02:24:32 +0100

    test/rand.c: new testing untility for rand_dh() function

--- test/rand.c
+++ test/rand.c
@@ -1,9 +1,35 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include <libdh/dhmisc.h>
+//#include <stdlib.h>
+//#include <sys/time.h>
+//#include <time.h>
 
+
+/*
+unsigned long rand_dh() {
+	unsigned long ret_rand;
+	struct timeval tod;
+	//float rand_operative;
+	unsigned long time;
+
+	gettimeofday(&tod, 0);
+	printf(" microseconds: %lu\n", tod.tv_usec);
+	time = tod.tv_usec;
+	time = time >> 1;
+	gettimeofday(&tod, 0);
+	time -= (tod.tv_usec >> 2);
+	time /= 100;
+	gettimeofday(&tod, 0);
+	time += (tod.tv_usec / 100);
+	time /= 100;
+	ret_rand = time;
+
+	return ret_rand;
+}
+*/
 
 int main(int argc, char **argv){
-	printf("%i\n", rand() % 100 + 1);
+	printf("\t%lu\n", rand_dh());
 	return 0;
 }
+
