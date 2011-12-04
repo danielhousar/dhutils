6142a495fa010c871a0d546b28cc576072025b48
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-12-04 23:16:02 +0100

    test/convert.c: updated calls to recent libdh API and ABI

--- test/convert.c
+++ test/convert.c
@@ -4,10 +4,14 @@
 #include <libdh/dhmisc.h>
 
 int main () {
+	char pole[64];
+	//sprintf(pole, "%f", 2.025);
+	ui2bins_t_dh(pole, 20);
 	printf("li = %li\n", shexrep_to_li_dh("0xaf"));
 	printf("%x\n", 175);
 	printf("fd = %f\n", sdecrep_to_fd_dh("2.0"));
-	printf("ui = %u\n", sbinrep_to_ui_dh("00011000"));
-	printf("bin 20 = %s\n", ui_to_binrep_dh(20));
+	printf("ul = %lu\n", sbinrep_to_ul_dh("00011000"));
+	printf("bin 20 = %s\n", pole);
+	//printf("bin 20 = %s\n", ui_to_binrep_dh(pole,20));
 	return 0;
 }
