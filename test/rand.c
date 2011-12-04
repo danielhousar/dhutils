2e95fd2f748438b2254661a742c54b47a35f9b45
Daniel Housar <daniel.h.080490.cs@gmail.com>
2011-12-05 00:04:08 +0100

    test/rand.c: removed commented code

--- test/rand.c
+++ test/rand.c
@@ -1,32 +1,5 @@
 #include <stdio.h>
 #include <libdh/dhmisc.h>
-//#include <stdlib.h>
-//#include <sys/time.h>
-//#include <time.h>
-
-
-/*
-unsigned long rand_dh() {
-	unsigned long ret_rand;
-	struct timeval tod;
-	//float rand_operative;
-	unsigned long time;
-
-	gettimeofday(&tod, 0);
-	printf(" microseconds: %lu\n", tod.tv_usec);
-	time = tod.tv_usec;
-	time = time >> 1;
-	gettimeofday(&tod, 0);
-	time -= (tod.tv_usec >> 2);
-	time /= 100;
-	gettimeofday(&tod, 0);
-	time += (tod.tv_usec / 100);
-	time /= 100;
-	ret_rand = time;
-
-	return ret_rand;
-}
-*/
 
 int main(int argc, char **argv){
 	printf("\t%lu\n", rand_dh());
