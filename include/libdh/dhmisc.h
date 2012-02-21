95deccd14b08a7116c720321ced48d6df43703b4
Daniel Housar <daniel.h.080490.cs@gmail.com>
2012-02-21 21:19:47 +0100

    include/: updated headers

--- include/libdh/dhmisc.h
+++ include/libdh/dhmisc.h
@@ -8,6 +8,7 @@
  extern void ui2bins_t_dh(char *p, unsigned int a);
  extern char *ui2bins_dh(char *p, unsigned int a);
  extern unsigned long rand_dh();
+ extern void wait (int seconds)
 
 #endif
 
